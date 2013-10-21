e93d42f281abf605cb75846d59cfd1d281564ec3
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2013-10-20 20:18:44 -0400

    Started working on apius presentation

--- Sherbrooke2013-cython/sinc/sinc_solution.pyx
+++ Sherbrooke2013-cython/sinc/sinc_solution.pyx
@@ -4,3 +4,12 @@ cpdef double sinc_kernel(double x):
     if -0.01 < x < 0.01:
         return 1.0
     return sin(x) / x
+
+def sinc_kernel(x):
+
+	cdef: 
+		double x_tmp = x
+
+	if -0.01 < x_tmp < 0.01:
+        return 1.0
+    return sin(x_tmp) / x_tmp		
