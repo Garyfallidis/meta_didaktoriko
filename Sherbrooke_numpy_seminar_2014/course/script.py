43ca36d03aeb145a7fe12d0b919bc24e4ab7a604
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2014-02-09 19:13:27 -0500

    Added line profiling example

---
+++ Sherbrooke_numpy_seminar_2014/course/script.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def f2(a, b):
+    f1(a)
+
+
+def f1(a):
+    a = np.array([1, 0, 1])
+    t = 1
+    1/0
+
+
+if __name__ == '__main__':
+
+    a = 2
+
+    print(a)
+
+    f2(a, 2)
+
