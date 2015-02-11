004887d8b07259f53a83721beb8eea031cf13458
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2015-02-10 22:32:00 -0500

    New Numpy course

---
+++ Sherbrooke_numpy_seminar_2015/course/script.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+def f2(a, b):
+    f1(a)
+
+#@profile
+def f1(a):
+    a = np.array([1, 0, 1])
+    t = 1
+
+
+
+def plot_test_pylab():
+
+    plot(np.arange(12))
+
+def plot_test_pyplot():
+
+    import matplotlib.pyplot as plt
+
+    plt.plot(np.arange(12))
+
+
+if __name__ == '__main__':
+
+    a = 2
+
+    print(a)
+
+    f2(a, 2)
+
+    #plot_test_pylab()
+    #plot_test_pyplot()
+
