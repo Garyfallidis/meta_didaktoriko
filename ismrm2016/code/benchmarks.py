c3f7561d20a288967c7d0b6895c8ae3517264047
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2015-11-08 20:24:11 -0500

    Updated test

--- ismrm2016/code/benchmarks.py
+++ ismrm2016/code/benchmarks.py
@@ -1,69 +1,84 @@
 import numpy as np
 import nibabel as nib
 from dipy.viz import actor, window
-from dipy.segment.clustering import QuickBundles
-from dipy.segment.clusteringspeed import QuickBundlesX
+from dipy.segment.clustering import QuickBundles, QuickBundlesX
 from dipy.segment.metric import AveragePointwiseEuclideanMetric
 from dipy.tracking.streamline import set_number_of_points
 from time import time
+from ipdb import set_trace
 
 
 dname = '/home/eleftherios/Data/Elef_Test_RecoBundles/'
 fname = dname + 'tracts.trk'
+fname_npz = dname + 'tracts.npz'
+
+
+def show_streamlines(streamlines):
+    renderer = window.Renderer()
+    bundle_actor = actor.line(streamlines)
+    renderer.add(bundle_actor)
+    window.show(renderer)
+
+# t = time()
+# trkfile = nib.streamlines.load(fname)
+# print('Loading time {}'.format(time()-t))
+# streamlines = trkfile.streamlines
+# nib.streamlines.utils.save_compact_list(fname_npz, trkfile.streamlines)
+
+# loading time improved from 85.11sec  to 13.29 usin npz and then 4.5 seconds!!
 
 t = time()
-trkfile = nib.streamlines.load(fname)
+streamlines = nib.streamlines.utils.load_compact_list(fname_npz)
 print('Loading time {}'.format(time()-t))
 
-streamlines = trkfile.streamlines
+print('Total number of streamlines {}'.format(len(streamlines)))
 
-nb = 100000
-streamlines = streamlines[:nb]
 
+t = time()
 rstreamlines = set_number_of_points(streamlines, 20)
+dt = time() - t
+print('Resampling time {}'.format(dt))
+print('\n')
 
-#renderer = window.Renderer()
-#bundle_actor = actor.line(streamlines)
-#renderer.add(bundle_actor)
+nb_range = [10**5, 2 * 10**5, 3 * 10**5, 4 * 10**5, 5 * 10**5, 6 * 10**5]
 
-#window.show(renderer)
+qb_times = []
+qbx_times = []
 
-thresholds = [30, 20, 15]
+for nb in nb_range:
 
-t = time()
-qbx = QuickBundlesX(rstreamlines[0].shape,
-                    thresholds, AveragePointwiseEuclideanMetric())
+    print('# Current size is {}'.format(nb))
+    print('\n')
 
-print(qbx)
+    rstreamlines_part = rstreamlines[:nb]
 
-for i, s in enumerate(rstreamlines):
-    print "\nInserting streamline {}".format(i)
-    qbx.insert(s.astype('f4'), np.int32(i))
-    # print(qbx)
+    thresholds = [30, 25, 20, 15]
 
-print('QBX time {}'.format(time()-t))
+    t = time()
+    qbx = QuickBundlesX(thresholds, metric=AveragePointwiseEuclideanMetric())
+    qbx_clusters = qbx.cluster(rstreamlines_part)
+    dt = time() - t
+    print(' QBX time {}'.format(dt))
+    qbx_times.append(dt)
+    qbx_clusters_1 = qbx_clusters.get_clusters(1)
+    qbx_clusters_2 = qbx_clusters.get_clusters(2)
+    qbx_clusters_3 = qbx_clusters.get_clusters(3)
+    qbx_clusters_4 = qbx_clusters.get_clusters(4)
+
+    print(' First level clusters {}'.format(len(qbx_clusters_1)))
+    print(' Second level clusters {}'.format(len(qbx_clusters_2)))
+    print(' Third level clusters {}'.format(len(qbx_clusters_3)))
+    print(' Third level clusters {}'.format(len(qbx_clusters_4)))
+    print('\n')
+
+    t = time()
+    qb = QuickBundles(thresholds[-1], metric=AveragePointwiseEuclideanMetric())
+    qb_clusters = qb.cluster(rstreamlines_part)
+    dt = time() - t
+    print(' QB time {}'.format(dt))
+    qb_times.append(dt)
+    print(' Clusters {}'.format(len(qb_clusters)))
+    print('\n')
 
 
-
-t = time()
-qbx = QuickBundlesX(rstreamlines[0].shape,
-                    thresholds, AveragePointwiseEuclideanMetric())
-
-print(qbx)
-
-for i, s in enumerate(rstreamlines):
-    print "\nInserting streamline {}".format(i)
-    qbx.insert(s.astype('f4'), np.int32(i))
-    # print(qbx)
-
-print('QBX time {}'.format(time()-t))
-
-t = time()
-
-qb = QuickBundles(thresholds[-1], metric=AveragePointwiseEuclideanMetric())
-qb.cluster(rstreamlines)
-
-print('QB time {}'.format(time()-t))
-
-from ipdb import set_trace
 set_trace()
